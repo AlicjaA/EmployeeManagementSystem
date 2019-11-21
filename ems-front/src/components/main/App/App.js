98c430396e58f6e0afc1f6ab6e869227f3945c25
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-21 22:29:09 +0100

    Add eslint to project

--- ems-front/src/components/main/App/App.js
+++ ems-front/src/components/main/App/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import Home from '../../scenes/Home';
+import {  
+  Route, 
+  Switch, 
+  BrowserRouter, 
+} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import Home from '../../scenes/Home';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- ems-front/src/components/scenes/Home/Home.js
+++ ems-front/src/components/scenes/Home/Home.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-    <Link to="/">Home</Link>
+        <Link to="/">Home</Link>
     </>
   );
 }

ba11bf80521dc0413dcb6e7a22cc9ef4125c14eb
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-21 21:54:01 +0100

    Add react-router-dom structore

--- ems-front/src/components/main/App/App.js
+++ ems-front/src/components/main/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Home from '../../scenes/Home';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-   <>
-   
-   </>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
   );
 }
 

7978af0e4f8b8f341e30709cf0f1e835b3c32a2c
Patryk Janicki <kontakt.janicki@gmail.com>
2019-11-22 21:19:58 +0100

    Add node sass library

--- ems-front/src/components/main/App/App.js
+++ ems-front/src/components/main/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import {  
-  Route, 
-  Switch, 
+import {
+  Route,
+  Switch,
   BrowserRouter,
 } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
 import Home from '../../scenes/Home';
 import Header from '../Header';
+import '../../../assets/scss/index.scss';
 
 function App() {
   return (

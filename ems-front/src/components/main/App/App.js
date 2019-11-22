d9ca2b4dcde393ff0c48b34080db5999eacbb114
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-22 19:47:27 +0100

    Add Navbar

--- ems-front/src/components/main/App/App.js
+++ ems-front/src/components/main/App/App.js
@@ -1,15 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {  
   Route, 
   Switch, 
-  BrowserRouter, 
+  BrowserRouter,
+  Link,
 } from 'react-router-dom';
+import { 
+  Navbar,
+  Nav,
+  NavbarBrand,
+  NavItem,
+  Collapse,
+  NavbarToggler,
+} from 'reactstrap';
+import NavLink from '../App/NavLink/NavLink';
 import 'bootstrap/dist/css/bootstrap.css';
 import Home from '../../scenes/Home';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <BrowserRouter>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand>
+          <Link to="/">EMS</Link>
+        </NavbarBrand>
+        <NavbarToggler onClick={() => setIsOpen(!isOpen)} />
+        <Collapse isOpen={isOpen} navbar>
+        <Nav className="mr-auto" navbar>
+          <NavItem>
+           <NavLink exact to="/aaa">AAA</NavLink>
+          </NavItem>
+          <NavItem>
+           <NavLink exact to="/bbb">BBB</NavLink>
+          </NavItem>
+          <NavItem>
+           <NavLink exact to="/ccc">CCC</NavLink>
+          </NavItem>
+        </Nav>
+        </Collapse>
+      </Navbar>
+
       <Switch>
         <Route exact path="/" component={Home} />
       </Switch>
--- ems-front/src/components/scenes/Home/Home.js
+++ ems-front/src/components/scenes/Home/Home.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from 'reactstrap';
 
 export default function Home() {
   return (
     <>
-        <Link to="/">Home</Link>
-        
-        <Button color="danger">Danger!</Button>
+      Home
     </>
   );
 }

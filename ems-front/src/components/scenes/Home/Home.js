247570dd874d1b7ae9af9e4ebdb2148e8b29d977
KJachimstal <40403722+KJachimstal@users.noreply.github.com>
2019-11-22 18:09:10 +0100

    Added reactstrap library

--- ems-front/src/components/scenes/Home/Home.js
+++ ems-front/src/components/scenes/Home/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from 'reactstrap';
 
 export default function Home() {
   return (
     <>
         <Link to="/">Home</Link>
+        
+        <Button color="danger">Danger!</Button>
     </>
   );
 }

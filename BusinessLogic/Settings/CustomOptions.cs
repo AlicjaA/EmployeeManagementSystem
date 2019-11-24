5107dd48ad39fbf567fac23738ede3943458f900
gbeznicki <g.beznicki@gmail.com>
2019-11-24 21:45:15 +0100

    Code cleanup and changing ISettings into options class according to documentation #9

---
+++ BusinessLogic/Settings/CustomOptions.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessLogic.Settings
+{
+    //https://docs.microsoft.com/pl-pl/aspnet/core/fundamentals/configuration/options?view=aspnetcore-3.0 
+    public class CustomOptions
+    {
+    }
+}

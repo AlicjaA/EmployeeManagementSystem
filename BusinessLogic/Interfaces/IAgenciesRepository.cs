448b1cc8d5e6c71a9bc62861ce1a6e4926f29e8d
gbeznicki <g.beznicki@gmail.com>
2019-12-01 13:25:16 +0100

    added agencies repository #9

---
+++ BusinessLogic/Interfaces/IAgenciesRepository.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Interfaces
+{
+    /// <summary>
+    /// Repository for Agencies and Ranges Management. 
+    /// Consists of CRUD operations.
+    /// </summary>
+    interface IAgenciesRepository
+    {
+        DbContext dbContext { get; }
+        void CreateAgency();
+        void RemoveAgency(int agencyId);
+        void RemoveAgency(Agency agency);
+        void UpdateAgency(int agencyIdToUpdate, Agency agencyUpdated);
+        void UpdateAgency(Agency agencyToUpdate, Agency agencyUpdated);
+        void DeleteAgency(int agencyId);
+        void DeleteAgency(Agency agency);
+
+        void CreateRange();
+        void RemoveRange(int rangeId);
+        void RemoveRange(Range range);
+        void UpdateRange(int rangeToUpdate, Range rangeUpdated);
+        void UpdateRange(Range rangeToUpdate, Range rangeUpdated);
+        void DeleteRange(int rangeId);
+        void DeleteRange(Range range);
+
+    }
+}

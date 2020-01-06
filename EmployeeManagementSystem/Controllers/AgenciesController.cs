2ab5cb91bef4b2d0a02eb16b50f5150b05f265ac
gbeznicki <g.beznicki@gmail.com>
2020-01-06 16:25:14 +0100

    added simple agencies controller

---
+++ EmployeeManagementSystem/Controllers/AgenciesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusinessLogic;
+using BusinessLogic.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeManagementSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgenciesController : ControllerBase
+    {
+        private RepositoryBase<Agency> repository = new RepositoryBase<Agency>(new EmployeeManagementSystemContext());
+        // GET: api/Agencies
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Agencies/5
+        [HttpGet("{id}", Name = "Get")]
+        public Agency Get(int id)
+        {
+            return repository.GetById(id);
+        }
+
+        // POST: api/Agencies
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Agencies/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
